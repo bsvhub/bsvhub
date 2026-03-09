4db1ce4db7647212d30016415d2533739bca94c2
bsvhub <bsvhub@proton.me>
2026-03-10 02:24:43 +0800

    Delete script/counter.js

--- script/counter.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  fetch("/api/count")
-    .then(function (res) { return res.json(); })
-    .then(function (data) {
-      var el = document.getElementById("visit-counter");
-      if (el) el.textContent = Number(data.count).toLocaleString();
-    });
-})();
